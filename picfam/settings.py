31a6448ec16256c767f057306b2b01bcb10c22df
HardikSoni11 <hsoni1096@gmail.com>
2020-10-05 07:29:54 +0530

    crispy_forms, password recovery email added

--- picfam/settings.py
+++ picfam/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'crispy_forms',
+
+    # local
+    'users.apps.UsersConfig',
+    'feed.apps.FeedConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +126,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# this will make crispy-forms use bootstrap4 instead of default 3 version
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home' # return to url after login
+LOGIN_URL = 'login' # url name used which has to be searched in url list for display of login page.
+
+
+# DEFAULT_FROM_EMAIL = 'your_custom_email_account'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('hsoni1096@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('.h.s.c.R@FL')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
